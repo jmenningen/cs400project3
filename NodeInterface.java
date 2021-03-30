9dbacad832c73c316d66978259b1eefaca7d0219
Raea <80125280+raeafr7@users.noreply.github.com>
2021-03-30 13:49:10 -0500

    changed NodeInterface.java to fit xin's data

--- NodeInterface.java
+++ NodeInterface.java
@@ -1,4 +1,14 @@
 
 public interface NodeInterface {
 
+  public boolean connectTo(Building<Integer> b);
+  public void addNeighbor(Building<Integer> b, int weight);
+  public int getNumber();  
+  public String getName();  
+  public boolean hasDining();  
+  public boolean hasLibraries();  
+  public boolean hasParking();  
+  public HashMap<Building<Integer>, Integer> getNeighbors();
+  public int getDistanceTo(Building<Integer> b);
+
 }
